f5ee4ad904b251cb8278f00a35a4dc01b38cb557
Frederik A. Rafn <far@2doit.nu>
2022-12-15 10:57:32 +0100

    Api to REAl Life server

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import { IconComponentProvider, Icon, ActivityIndicator } from "@react-native-ma
 import { Button,IconButton  } from "@react-native-material/core";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { useNavigation } from "@react-navigation/native";
+import {axios} from 'axios';
 const Stack = createStackNavigator()
 let Userid = '';
 let UderKateId = '';
@@ -54,12 +55,33 @@ export default function App({navigation}) {
 function Login({navigation}){
   const[username,setUsername]=useState('');
   const[password,setPassword]=useState('');
-  const onLogin = () => {
-    Alert.alert("Fuck");
-    navigation.navigate("Kategori")
+  const onLogin = async() => {
+   /* const respons =  await fetch('http://god.rundstykker.macd.dk/api/Bruger/HentBrugerGUID', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "Username": username,
+          "Password": password,
+        })
+      });
+      const json = await respons.text();
+      const res = await respons.status;
+      console.log(json);
+      console.log(res);
+      if (res == 400) {
+        Alert.alert(json)
+      }
+      if (res==200) {
+        Alert.alert("Loggoetid")
+        navigation.navigate("Kategori") 
+      }*/
+      Alert.alert("Logget ind")
   }
   const onRegister = () =>{
-    Alert.alert("mega fucker");
+    Alert.alert("Hey eksample 3");
     Userid ="Test1";
     navigation.navigate("RegisterUser")
 
@@ -81,8 +103,6 @@ function Login({navigation}){
         
         <CustomButton style={{width:'80%'}} text="Login" onPress={onLogin}/>
         <CustomButton style={{width:'50%'}} text="register" onPress={onRegister}/>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -156,24 +176,144 @@ function Login({navigation}){
 function User(){
   return(
     <View>
-      <Text></Text>
+      <Text>User</Text>
     </View>
   )
 }
-function UnderKate(){
+function UnderKate({navigation}){
+  const QrScan = () =>{
+    navigation.navigate("QrScanner")
+  }
+  const UnderKategori = () =>{
+    navigation.navigate("VareInfo")
+  }
   
   const [data,setData]=useState([])
+  const screenHeight = Dimensions.get('window').height
   const [loading,setloading]=useState(true)
+  const getdata = async () => {
+    try {
+      const respons = await fetch('https://api.openbrewerydb.org/breweries');
+      const json = await respons.json();
+      setData(json);
+    }
+    catch(error){
+      console.error("lolr");
+    }
+    finally{
+      setloading(false);
+    }
+  } 
+  useEffect(()=>{
+    getdata();
+  },[])
   return(
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+    <View style={{flex:1,width:'100%',height:'85%', padding:5,}}>
+    <ScrollView style={{  }}>
+          
+          {
+            
+            loading ? (<Text>Loading ....</Text>):(
+            <FlatList
+              keyExtractor={(item)=>item.id}
+              data={data}
+              renderItem={({item})=>(
+                  <View style={styles.boxes}>
+                    <TouchableOpacity style={{opacity:1,backgroundColor: 'rgba(52, 52, 52, 0.8)',height:'100%',}} onPress={UnderKategori}>
+                      <Text style={{textAlign:'center',fontSize:18,marginTop:50,width:'100%', alignItems:'center',justifyContent:'center'}}>{item.name}</Text>
+                    </TouchableOpacity>
+                  </View>
+              )}
+            />
+          )
+          } 
+        </ScrollView>
+    </View>
+      
+        
+        <View style={styles.buttonContainer}>
+              <CustomButton  text="Login" onPress={QrScan}/>
+        </View>
     </View>
   )
 }
-function Varer(){
+
+function Varer({navigation}){
+
+  const [data,setData]=useState([])
+  const screenHeight = Dimensions.get('window').height
+  const [loading,setloading]=useState(true)
+  const getdata = async () => {
+    try {
+      const respons = await fetch('https://api.openbrewerydb.org/breweries');
+      const json = await respons.json();
+      setData(json);
+    }
+    catch(error){
+      console.error("lolr");
+    }
+    finally{
+      setloading(false);
+    }
+  } 
+  useEffect(()=>{
+    getdata();
+  },[])
+  const Varespecifik = ()=>{
+    navigation.navigate('VareInfo')
+  }
+
+  /*const getdata = async () => {
+    try {
+      console.log("virker")
+      const respons = await fetch('https://192.168.87.25:7110/api/Vare/CheckQr',{
+        method:'GET',
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      });
+      const json = await respons.json();
+      setData(json);
+    }
+    catch(error){
+      console.error(error);
+    }
+    finally{
+      setloading(false);
+    }
+  } 
+  useEffect(()=>{
+    getdata();
+  })*/
   return(
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+    <View style={{flex:1,width:'100%',height:'85%', padding:5,}}>
+    <ScrollView style={{  }}>
+          
+          {
+            
+            loading ? (<Text>Loading ....</Text>):(
+            <FlatList
+              keyExtractor={(item)=>item.id}
+              data={data}
+              renderItem={({item})=>(
+                  <View style={styles.boxes}>
+                    <TouchableOpacity style={{opacity:1,backgroundColor: 'rgba(52, 52, 52, 0.8)',height:'100%',}} onPress={Varespecifik}>
+                      <Text style={{textAlign:'center',fontSize:18,marginTop:50,width:'100%', alignItems:'center',justifyContent:'center'}}>{item.name}</Text>
+                    </TouchableOpacity>
+                  </View>
+              )}
+            />
+          )
+          } 
+        </ScrollView>
+    </View>
+      
+        
+        <View style={styles.buttonContainer}>
+              <CustomButton  text="Login" />
+        </View>
     </View>
   )
 }
@@ -181,10 +321,12 @@ function Varer(){
 //
 // Qr Scanner Funktion
 //
-function QrScanner(){
+function QrScanner({navigation}){
   const[HasPermission, setHasPermission] = useState(null);
   const[scanned,SetScanned] = useState(false);
   const[text, setText]=useState("Not yet scanned");
+  const[vareid,setvareid]=useState([])
+  
 
   const askForCameraPermission = () => {
     (async () => {
@@ -198,10 +340,30 @@ function QrScanner(){
   }, []);
 
 
-  const handleBarCodeScanned = ({type,data}) =>{
+  const handleBarCodeScanned = async ({type,data}) =>{
     SetScanned(true);
     setText(data);
-    console.log('Type: '+type+ '\nData: ' +data)
+    try{
+      console.log("TEST");
+      //const respons = fetch('http://god.rundstykker.macd.dk/api/Vare/TEST')
+      //const test2 = await axios.get('https://api.openbrewerydb.org/breweries')
+      //https://localhost:7110/api/Vare/CheckQr
+      //console.log("LOL")
+      //console.log(respons)
+      //setvareid(vareidJson);
+    }
+   catch(res){
+      console.log(res)
+    }
+    if (data != null) {
+      if (vareid[0]) {
+        navigation.navigate("Varer")
+      }
+    }
+    else{
+      console.log('no data')
+    }
+    //console.log('Type: '+type+ '\nData: ' +data)
   }
 
   if(HasPermission ===null){
@@ -240,37 +402,68 @@ function RegisterUser({navigation}){
     Alert.alert(Userid);
     navigation.navigate("Login")
   }  
-  const Opret = () => {
-
-    navigation.navigate("Kategori")
+  const Opret = async () => {
+    try{
+      /*const respons =  await fetch('http://god.rundstykker.macd.dk/api/Bruger/RegitrereBruger', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "Username": usernamee,
+          "Password": passworde,
+          "Email":Emaile,
+          "Fornavn":Fornavne,
+          "Efternavn":EfterNavne,
+          "Role":"normal"
+        })
+      });
+      const json = await respons.text();
+      const res = await respons.status;
+      console.log(json);
+      console.log(res);
+      if (res == 400) {
+        Alert.alert(json)
+      }
+      if (res==200) {
+        Alert.alert("User Oprettet")
+        navigation.navigate("Kategori") 
+      }*/
+      Alert.alert("User Oprettet")
+      navigation.navigate("Kategori") 
+    }
+   catch(error){
+      console.log(error)
+   }
   }
-  const[Email,setEmail]=useState('');
-  const[username,setUsername]=useState('');
-  const[password,setPassword]=useState('');
-  const[RepeatPassword,setRepeatPassword]=useState('');
-  const[Fornavn,setFornavn]=useState('');
-  const[EfterNavn,setEfterNavn]=useState('');
+  const[Emaile,setEmail]=useState('');
+  const[usernamee,setUsername]=useState('');
+  const[passworde,setPassword]=useState('');
+  const[RepeatPassworde,setRepeatPassword]=useState('');
+  const[Fornavne,setFornavn]=useState('');
+  const[EfterNavne,setEfterNavn]=useState('');
   return(
     <View style={styles.container}>
       <Text style={{right:130}}>Email</Text>
       <CustomInput 
         style={styles.imput}
         placeholder="Email"
-        value={Email}
+        value={Emaile}
         setValue={setEmail}
       />
       <Text style={{right:115}}>Username</Text>
       <CustomInput 
         style={styles.imput}
         placeholder="Username"
-        value={username}
+        value={usernamee}
         setValue={setUsername}
       />
       <Text style={{right:115}}>Password</Text>
       <CustomInput 
         style={styles.imput}
         placeholder="Password"
-        value={password}
+        value={passworde}
         setValue={setPassword}
         secureTextEntry
       />
@@ -278,7 +471,7 @@ function RegisterUser({navigation}){
       <CustomInput 
         style={styles.imput}
         placeholder="Gentag Password"
-        value={RepeatPassword}
+        value={RepeatPassworde}
         setValue={setRepeatPassword}
         secureTextEntry
       />
@@ -286,14 +479,14 @@ function RegisterUser({navigation}){
       <CustomInput 
         style={styles.imput}
         placeholder="Fornavn"
-        value={Fornavn}
+        value={Fornavne}
         setValue={setFornavn}
       />
       <Text style={{right:115}}>Efter navn</Text>
       <CustomInput 
         style={styles.imput}
         placeholder="Efter navn"
-        value={EfterNavn}
+        value={EfterNavne}
         setValue={setEfterNavn}
       />
       <View style={styles.buttonContainer}>
@@ -311,9 +504,29 @@ function UserFavorits(){
   )
 }
 function VareInfo(){
+  const [data,setData]=useState([])
+  const screenHeight = Dimensions.get('window').height
+  const [loading,setloading]=useState(true)
+  const tjek = async () => {
+    try {
+      //const respons = await fetch('http://god.rundstykker.macd.dk/api/Vare/TEST',{method:'GET', headers:{'Accept': '*/*','Content-Type': 'application/x-www-form-urlencoded',}});
+      /*const json = await respons.text();
+      const res = await respons.status;
+      console.log(json);
+      console.log(res);*/
+      //setData(json);
+      console.log("Tjek")
+    }
+    catch(error){
+      console.error(error);
+    }
+    finally{
+      setloading(false);
+    }
+  } 
   return(
     <View>
-      <Text></Text>
+    <Button onPress={tjek}></Button>
     </View>
   )
 }
